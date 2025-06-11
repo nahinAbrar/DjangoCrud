00edc9a0ba48ed060538585f747521ab847f8a3c
Asm Nahin <asmnahin21@gmail.com>
2025-06-11 17:37:39 +0600

    api setups

--- server/bookBackend/api/views.py
+++ server/bookBackend/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializer import BookSerializer
 
-# Create your views here.
+@api_view(["GET"])
+def get_books(request):
+    books = Book.objects.all()
+    serializedData = BookSerializer(books, many=True).data
+    return Response(serializedData)
+
+@api_view(["POST"])
+def create_book(request):
+    data = request.data
+    serializer = BookSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
