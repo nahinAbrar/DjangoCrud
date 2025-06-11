bbd41da36c5e18b8942d10c26e9ce728635854cd
Asm Nahin <asmnahin21@gmail.com>
2025-06-11 20:32:43 +0600

    Complete

--- server/bookBackend/api/views.py
+++ server/bookBackend/api/views.py
@@ -4,11 +4,13 @@ from rest_framework import status
 from .models import Book
 from .serializer import BookSerializer
 
+
 @api_view(["GET"])
 def get_books(request):
     books = Book.objects.all()
     serializedData = BookSerializer(books, many=True).data
     return Response(serializedData)
+
 
 @api_view(["POST"])
 def create_book(request):
@@ -18,3 +20,22 @@ def create_book(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(["PUT", "DELETE"])
+def book_detail(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "DELETE":
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == "PUT":
+        data = request.data
+        serializer = BookSerializer(book, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_404_NOT_FOUND)
